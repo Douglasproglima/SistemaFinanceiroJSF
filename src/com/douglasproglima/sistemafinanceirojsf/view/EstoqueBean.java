31a3a590dcac1fdea3e403cd37c2bdbc2860014d
Douglas Lima <douglasproglima@gmail.com>
2016-07-25 11:49:23 -0300

    4.4 - Ajuste no Bean Estoque e ItemEstoque

---
+++ src/com/douglasproglima/sistemafinanceirojsf/view/EstoqueBean.java
@@ -0,0 +1,33 @@
+package com.douglasproglima.sistemafinanceirojsf.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class EstoqueBean{
+	private ItemEstoqueBean itemEstoqueBean = new ItemEstoqueBean();
+	private List<ItemEstoqueBean> itensEstoque = new ArrayList<ItemEstoqueBean>();
+	
+
+	//Métodos Getters
+	public ItemEstoqueBean getItemEstoqueBean() {
+		return itemEstoqueBean;
+	}
+	
+	public List<ItemEstoqueBean> getItensEstoque() {
+		return itensEstoque;
+	}
+	
+	//Métodos Funcionais
+	public void incluir(){
+		this.itensEstoque.add(this.itemEstoqueBean);
+		
+		//Instância um novo itemEstoque apenas para limpar o grid
+		this.itemEstoqueBean = new ItemEstoqueBean();
+	}
+
+}
---
+++ src/com/douglasproglima/sistemafinanceirojsf/view/ItemEstoqueBean.java
@@ -0,0 +1,51 @@
+package com.douglasproglima.sistemafinanceirojsf.view;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class ItemEstoqueBean {
+	private Integer codigoProduto;
+	private String descricao;
+	private Short quantidade;
+	private BigDecimal valorUnitario;
+	private Date dataChecagem;
+	
+	//Métodos Getters e Setters
+	public Integer getCodigoProduto() {
+		return codigoProduto;
+	}
+	public void setCodigoProduto(Integer codigoProduto) {
+		this.codigoProduto = codigoProduto;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public Short getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(Short quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	public BigDecimal getValorUnitario() {
+		return valorUnitario;
+	}
+	public void setValorUnitario(BigDecimal valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+	
+	public Date getDataChecagem() {
+		return dataChecagem;
+	}
+	public void setDataChecagem(Date dataChecagem) {
+		this.dataChecagem = dataChecagem;
+	}
+}
