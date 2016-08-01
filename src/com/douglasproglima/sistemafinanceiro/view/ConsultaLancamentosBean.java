b2d6c4b5c621a9bbb6f8dd0d378c55b803e0302a
Douglas Lima <douglasproglima@gmail.com>
2016-08-01 16:16:11 -0300

    5.7 Método Excluir

--- src/com/douglasproglima/sistemafinanceiro/view/ConsultaLancamentosBean.java
+++ src/com/douglasproglima/sistemafinanceiro/view/ConsultaLancamentosBean.java
@@ -4,17 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.criterion.Order;
 
 import com.douglasproglima.sistemafinanceiro.model.Lancamento;
+import com.douglasproglima.sistemafinanceiro.util.FacesUtil;
 import com.douglasproglima.sistemafinanceiro.util.HibernateUtil;
 
 @ManagedBean
 public class ConsultaLancamentosBean {
 	private List<Lancamento> lancamentos = new ArrayList<Lancamento>();
+	private Lancamento lancamentoSelecionado;
 	
 	@SuppressWarnings({ "unchecked", "static-access" })
 	@PostConstruct
@@ -26,7 +30,37 @@ public class ConsultaLancamentosBean {
 		sessao.close();
 	}
 	
+	public void excluir(){
+		if (this.lancamentoSelecionado.isPago()) {
+			FacesUtil.adicionarMensagem(FacesMessage.SEVERITY_ERROR, 
+										"O lançamento já foi pago. \nRegistro não pode ser excluído!", 
+										"O lançamento já foi pago. \nRegistro não pode ser excluído!");
+		}else{
+			Session sessao = HibernateUtil.getSessao();
+			Transaction transacao = sessao.beginTransaction();
+			
+			sessao.delete(this.lancamentoSelecionado);
+			
+			transacao.commit();
+			sessao.close();
+			
+			FacesUtil.adicionarMensagem(FacesMessage.SEVERITY_INFO, 
+										"Lançamento Excluído com sucesso!", 
+										"Lançamento Excluído com sucesso!");
+			
+			inicializar();
+		}
+	}
+	
+	//Métodos Bean Getters e Setters
 	public List<Lancamento> getLancamentos() {
 		return lancamentos;
+	}
+	
+	public Lancamento getLancamentoSelecionado() {
+		return lancamentoSelecionado;
+	}
+	public void setLancamentoSelecionado(Lancamento lancamentoSelecionado) {
+		this.lancamentoSelecionado = lancamentoSelecionado;
 	}
 }
