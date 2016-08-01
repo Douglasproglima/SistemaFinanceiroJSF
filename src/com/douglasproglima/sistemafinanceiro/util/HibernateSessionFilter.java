b1ea3a93d3de827896c7ab293c61e5a623178593
Douglas Lima <douglasproglima@gmail.com>
2016-08-01 16:53:52 -0300

    5.8 Adicionando Class HibernateSessionFilter com o Método doFilter
    Class necessária para abrir e fechar a conexão com o banco, abrir e
    fechar uma transação.
    
    Essa class será utilizada nos ManageBeans com as anotações de
    @ScopedXXXXX

---
+++ src/com/douglasproglima/sistemafinanceiro/util/HibernateSessionFilter.java
@@ -0,0 +1,53 @@
+package com.douglasproglima.sistemafinanceiro.util;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@WebFilter(servletNames={"Faces Servlet"})
+public class HibernateSessionFilter implements Filter{
+
+	@Override
+	public void doFilter(ServletRequest requisicao, ServletResponse resposta, FilterChain chain)
+			throws IOException, ServletException {
+		
+		Session sessao =  HibernateUtil.getSessao();
+		Transaction transacao = null;
+		
+		try {
+			//Inicia uma transação com o banco de dados;
+			transacao = sessao.beginTransaction();
+			
+			//Atribui a requisiçao a sessão do hibernate, será utilizado nos managesBean nas anotações de escopos.
+			requisicao.setAttribute("sessao", sessao);
+			
+			//Continua o processo depois da requisição
+			chain.doFilter(requisicao, resposta);
+			
+			transacao.commit();
+		} catch (Exception erro) {
+			if (transacao != null) {
+				transacao.rollback();
+			}	
+		}finally {
+			sessao.close();
+		}
+	}	
+	
+	@Override
+	public void destroy() {	
+	}
+
+	@Override
+	public void init(FilterConfig coniguracaoFiltro) throws ServletException {
+	}
+}
