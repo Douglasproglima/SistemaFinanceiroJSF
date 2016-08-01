2af93cb91efa4c68ed22770144eb8f2ff61bd74d
Douglas Lima <douglasproglima@gmail.com>
2016-08-01 17:12:31 -0300

    5.7 Adicionado Métod getAtributosDaRequisicao(String nomeAtributoSessaoDoFilter)
    Método responsável por retornar o objeto request da class
    HibernateSessionFilter, método doFilter(), que abre e fecha uma sessão e
    também abre e fecha uma transação com o banco de dados.

--- src/com/douglasproglima/sistemafinanceiro/util/FacesUtil.java
+++ src/com/douglasproglima/sistemafinanceiro/util/FacesUtil.java
@@ -2,7 +2,9 @@ package com.douglasproglima.sistemafinanceiro.util;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.application.FacesMessage.Severity;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 
 public class FacesUtil {
 	
@@ -10,5 +12,18 @@ public class FacesUtil {
 	public static void adicionarMensagem(Severity tipoMensagem, String msgResumida, String msgComDetalhes){
 		FacesContext.getCurrentInstance().addMessage(null, 
 		new FacesMessage(tipoMensagem, msgResumida, msgComDetalhes));
+	}
+	
+	public static Object getAtributosDaRequisicao(String nomeAtributoSessaoDoFilter){
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		
+		//A partir do contexto externo, consegue pegar o escopo de requisição
+		ExternalContext externalContext = facesContext.getExternalContext();  
+		
+		//Criando um objeto da API do Servlet
+		HttpServletRequest requisicao = (HttpServletRequest) externalContext.getRequest();
+		
+		//Todo o procedimento acima necessário para buscar o objeto request da Class
+		return requisicao.getAttribute(nomeAtributoSessaoDoFilter);
 	}
 }
