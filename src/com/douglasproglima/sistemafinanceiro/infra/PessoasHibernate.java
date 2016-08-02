cf8d53ee2f30182efa7c8ee6371bcc349179b4ed
Douglas Lima <douglasproglima@gmail.com>
2016-08-02 19:44:15 -0300

    5.9 Implementando a interface RepositorioPessoas na class PessoasHibernate
    Recebe a sessão pelo construtor (Poderia fazer pelo setSessao()), em
    resumo por injeção de dependência

---
+++ src/com/douglasproglima/sistemafinanceiro/infra/PessoasHibernate.java
@@ -0,0 +1,34 @@
+package com.douglasproglima.sistemafinanceiro.infra;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
+
+import com.douglasproglima.sistemafinanceiro.model.Pessoa;
+import com.douglasproglima.sistemafinanceiro.repositorio.RepositorioPessoas;
+
+public class PessoasHibernate implements RepositorioPessoas{
+
+
+	private Session sessao;
+	
+	//Recebe a sessão pelo construtor (Poderia fazer pelo setSessao()), em resumo por injeção de dependência
+	public PessoasHibernate(Session sessao){
+		this.sessao = sessao;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Pessoa> consultaTodas() {
+		
+		return sessao.createCriteria(Pessoa.class).addOrder(Order.asc("nome")).list();
+	}
+
+	@Override
+	public Pessoa consultaPorCodigo(Integer codigo) {
+
+		return (Pessoa) sessao.get(Pessoa.class, new Integer(codigo));
+	}
+
+}
