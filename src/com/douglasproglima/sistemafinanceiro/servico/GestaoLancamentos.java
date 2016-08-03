d0eb5e7f11f8af9d175eaca682c1d3ba437e39ee
Douglas Lima <douglasproglima@gmail.com>
2016-08-03 17:18:05 -0300

    5.10 Implementando Regras de Negocios/Nova Class GestaoLancamento
    11 - Criado o pacorte 'servico' e criado a class GestaoLancamento que
    contém a regra de negócio dos Lançamentos, essa class contém os métodos
    de negócio para salvar(), excluir() e editar() um novo registro de
    lançamento.

---
+++ src/com/douglasproglima/sistemafinanceiro/servico/GestaoLancamentos.java
@@ -0,0 +1,41 @@
+package com.douglasproglima.sistemafinanceiro.servico;
+
+import com.douglasproglima.sistemafinanceiro.model.Lancamento;
+import com.douglasproglima.sistemafinanceiro.repositorio.FichaDeLancamentos;
+
+public class GestaoLancamentos {
+
+	/*Boas Práticas
+	 * Em uma camada de regra de negócio pode acessar API do JSF, exemplo FacesContext, FacesMessages etc...
+	 * Não é aconselhavél colocar detalhes de API na class de negócio.
+	 * */
+	
+	private FichaDeLancamentos fichaDeLancamentos;
+	
+	//Injeção de dependência pelo construtor.
+	public GestaoLancamentos(FichaDeLancamentos fichaDeLancamentos){
+		this.fichaDeLancamentos = fichaDeLancamentos;
+	}
+
+	private boolean existeLancamentoIgual(Lancamento lancamento){
+		Lancamento lancamentoDuplicado = this.fichaDeLancamentos.comDadosIguais(lancamento);
+		
+		return  lancamentoDuplicado != null && !lancamentoDuplicado.equals(lancamento);
+	}	
+	
+	public void salvar(Lancamento lancamento) throws RegraNegocioException{
+		if (existeLancamentoIgual(lancamento)) {
+			throw new RegraNegocioException("Já existe um registro igual a este!");
+		}
+		
+		this.fichaDeLancamentos.guardar(lancamento);
+	}
+	
+	public void excluir(Lancamento lancamento) throws RegraNegocioException{
+		if (lancamento.isPago()) {
+			throw new RegraNegocioException("Lançamento pago não pode ser excluído!");
+		} else {
+			this.fichaDeLancamentos.remover(lancamento);
+		}
+	}
+}
