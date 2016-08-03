5ba97c5d4a0d45b5fd1a7339babbb8d57f4d868e
Douglas Lima <douglasproglima@gmail.com>
2016-08-03 17:21:24 -0300

    5.10 Implementando Regras de Negocios/Adicionado novo método comDadosIguais() na class LancamentosDAO
    16 - Implementado novo método comDadosIguais() da interfase
    FichaDeLancamentos() que verifica se algum registro já inserido com as
    mesmas informações a serem inseridos, impedindo a duplicidade de
    registros.

--- src/com/douglasproglima/sistemafinanceiro/repositorio/DAO/LancamentosDAO.java
+++ src/com/douglasproglima/sistemafinanceiro/repositorio/DAO/LancamentosDAO.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 
 import com.douglasproglima.sistemafinanceiro.model.Lancamento;
 import com.douglasproglima.sistemafinanceiro.repositorio.FichaDeLancamentos;
@@ -32,4 +33,14 @@ public class LancamentosDAO implements FichaDeLancamentos{
 		this.sessao.delete(lancamento);
 	}
 
+	@Override
+	public Lancamento comDadosIguais(Lancamento lancamento) {
+		return (Lancamento) this.sessao.createCriteria(Lancamento.class)
+						  			   .add(Restrictions.eq("tipo", lancamento.getTipo()))
+						  			   .add(Restrictions.eq("pessoa", lancamento.getPessoa()))
+						  			   .add(Restrictions.ilike("descricao", lancamento.getDescricao()))
+						  			   .add(Restrictions.eq("valor", lancamento.getValor()))
+						  			   .add(Restrictions.eq("dataVencimento", lancamento.getDataVencimento()))
+						  			   .uniqueResult();
+	}
 }
