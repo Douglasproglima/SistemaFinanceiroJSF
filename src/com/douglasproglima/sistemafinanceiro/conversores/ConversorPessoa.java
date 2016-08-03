f29331489506c363163606e37a91fd488e5e48a4
Douglas Lima <douglasproglima@gmail.com>
2016-08-03 13:19:53 -0300

    5.9 Pattern Repository/Alteração ConversorPessoa()
    6 - Adicionado o método getPessoas da class Repositorios para ocultar os
    dados da sessão e instanciamento da requisição.

--- src/com/douglasproglima/sistemafinanceiro/conversores/ConversorPessoa.java
+++ src/com/douglasproglima/sistemafinanceiro/conversores/ConversorPessoa.java
@@ -5,23 +5,24 @@ import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 
-import org.hibernate.Session;
-
 import com.douglasproglima.sistemafinanceiro.model.Pessoa;
-import com.douglasproglima.sistemafinanceiro.util.FacesUtil;
+import com.douglasproglima.sistemafinanceiro.repositorio.FichaDePessoas;
+import com.douglasproglima.sistemafinanceiro.util.Repositorios;
 
 //Este conversor não precisa espeficicar na página, porque estamos falando que ele irá sempre ser utilizado pela class pessoa
 @FacesConverter(forClass=Pessoa.class)
 public class ConversorPessoa implements Converter{
 
+	private Repositorios repositorios = new Repositorios();
+	
 	@Override
 	public Object getAsObject(FacesContext context, UIComponent componente, String valor) {
 		Pessoa retornoPessoa = null;
 		
 		if(valor != null){
-			Session sessao = (Session) FacesUtil.getAtributosDaRequisicao("sessaoMetodoDoFilter");
-
-			retornoPessoa = (Pessoa) sessao.get(Pessoa.class, new Integer(valor));
+			FichaDePessoas fichaDePessoas = repositorios.getPessoas();
+			
+			retornoPessoa = fichaDePessoas.consultaPorCodigo(new Integer(valor));
 		}
 		
 		return retornoPessoa;
