c60149e27eea72bb788d919209ef490b5cd12274
Douglas Lima <douglasproglima@gmail.com>
2016-08-03 17:20:32 -0300

    5.10 Implementando Regras de Negocios/adicionado novo método comDadosIguais() na interfase FichaDeLancamentos()
    15 - Alteração necessária para implementar a regra de negócio na class
    GestaoLancamento no método salvar() para bloquear a insersão de
    registros duplicados.

--- src/com/douglasproglima/sistemafinanceiro/repositorio/FichaDeLancamentos.java
+++ src/com/douglasproglima/sistemafinanceiro/repositorio/FichaDeLancamentos.java
@@ -7,6 +7,7 @@ import com.douglasproglima.sistemafinanceiro.model.Lancamento;
 public interface FichaDeLancamentos {
 	
 	public List<Lancamento> consultaTodos();
+	public Lancamento comDadosIguais(Lancamento lancamento);
 	public Lancamento guardar(Lancamento lancamento);
 	public void remover(Lancamento lancamento);
 }
