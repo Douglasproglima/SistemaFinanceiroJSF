caee8e8a887dbf6ff7f273bd8e3e9439fc5d8559
Douglas Lima <douglasproglima@gmail.com>
2016-08-22 10:44:18 -0300

    6.9 Implementando Segurança com Jaas/ Correção de Bugs

--- src/com/douglasproglima/sistemafinanceiro/view/SegurancaBean.java
+++ src/com/douglasproglima/sistemafinanceiro/view/SegurancaBean.java
@@ -1,32 +1,44 @@
 package com.douglasproglima.sistemafinanceiro.view;
 
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 
 import com.douglasproglima.sistemafinanceiro.util.FacesUtil;
 
+@ManagedBean
 public class SegurancaBean {
 	
 	private String usuario;
 	private String senha;
+
+	//Busca o http do ServletRequest
+	private HttpServletRequest getRequisicao(){
+		
+		FacesContext context = FacesContext.getCurrentInstance();
+		return (HttpServletRequest) context.getExternalContext().getRequest();
+	}	
 	
 	//Método de logar
 	public String logar(){
-		FacesContext context = FacesContext.getCurrentInstance();
-		
-		//Busca o http do Servlet
-		HttpServletRequest requisicao = (HttpServletRequest) context.getExternalContext().getRequest();
 		
 		try {
-			requisicao.login(usuario, senha);			
-			return "PaiginaInicial?faces=redirect=true";
+			this.getRequisicao().login(usuario, senha);			
+			return "PaginaInicial?faces=redirect=true";
 		} catch (Exception e) {
 			FacesUtil.adicionarMensagem(FacesMessage.SEVERITY_ERROR, 
 										FacesUtil.getMensagemI18N("msg_user_senha_nao_existem"), 
 										FacesUtil.getMensagemI18N("msg_user_senha_nao_existem"));
 			return null;
 		}
+	}
+	
+	public String sair() throws ServletException{
+		
+		this.getRequisicao().logout();
+		return "Login?faces=redirect=true";
 	}
 	
 	public String getUsuario() {
